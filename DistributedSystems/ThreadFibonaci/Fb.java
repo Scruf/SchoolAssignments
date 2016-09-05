fbd82c224e7adbf98f46f91edc6ed1c60cc70cc2
Scruf <ekozitski@hotmail.com>
2016-09-05 19:59:11 -0400

    Added Todo for Java

--- DistributedSystems/ThreadFibonaci/Fb.java
+++ DistributedSystems/ThreadFibonaci/Fb.java
@@ -1,4 +1,10 @@
 public class Fb{
+	/*
+			TODO:Create Monitot class
+			Validation output
+			Create Thread which will create Fb sequence
+
+	*/
 	public static void main (String []args){
 		System.out.println("hello World");
 	}
