15ffde454586b288f7c2a6a06541ea1b32e58154
Scruf <ekozitski@hotmail.com>
2016-09-13 00:02:19 -0400

    Added Fibonacci Numbers

--- DistributedSystems/ThreadFibonaci/Fib.java
+++ DistributedSystems/ThreadFibonaci/Fib.java
@@ -1,30 +1,85 @@
 import java.math.*;
-
-public class Fib{
+import java.io.*;
+/*
+	Implement a working version of compareTo
+*/
+public class Fib extends Thread{
+	//size of the list
 	public int n;
-	public int F[];
+	//first argument
+	public BigInteger a;
+	//second argumennt
+	public BigInteger b;
+	//array of fibonacci numbers
+	public BigInteger F[];
+	private Thread output;
+	private Thread calc;
 	Fib(int n){
 		this.n = n;
-		F = new int [n];
+		this.F = new BigInteger [n];
+	}
+	public void putValue(int i, BigInteger element){
+		F[0] = this.a;
+		F[1] = this.b;
+		if(F[i]!=null)
+			System.out.print("There is an element at this location");
+		else{
+			F[i] = element;
+		}
 	}
 
-	public void main(String[] args) {
-		if(args.length<3){
-			System.err.print("Number if arguments must account to 3\n");
+	public void run(){
+		
+		Thread compute = new Thread(new Runnable(){
+			public void run(){
+				
+			}
+		});
+		Thread output = new Thread(new Runnable(){
+			
+			public void run(){
+				System.out.print("I am thread two\n");
+			}
+		});
+		compute.start();
+		output.start();
+		try{
+			compute.join();
+			output.join();
+		}catch(InterruptedException ex){
+			System.err.print(ex);
 		}
+
+		
+	}
+
+
+
+
+	public static void main(String[] args) {
+		if (args.length<3){
+			System.err.print("Invalid number of argumnets\n");
+			System.exit(0);
+		}
+		Integer n = 0;
 		Integer a = 0;
 		Integer b = 0;
-		Integer c = 0;
 		try{
-			a = Integer.parseInt(args[0]);
-			b = Integer.parseInt(args[1]);
-			c = Integer.parseInt(args[2]);
-		}catch (Exception ex){
-			System.err.print("Invalid parameter format\n");
+			 n = Integer.parseInt(args[2]);
+			 a = Integer.parseInt(args[0]);
+			 b = Integer.parseInt(args[1]);
+			if(n<0){
+				System.err.print("N must be >= 0");
+				System.exit(0);
+			}
+		}catch(Exception ex){
+			System.err.print("Invalid arguments type");
+			System.exit(0);
 		}
-		if (c<0){
-			System.err.print("argument C must be greater than 0");
-		}
-	}
-	Fib fib = new Fib(0);
+		Fib fib = new Fib(n);
+		fib.a = BigInteger.valueOf(a);
+		fib.b = BigInteger.valueOf(b);
+	
+		fib.putValue(2,BigInteger.valueOf(5));
+	}	
 }
