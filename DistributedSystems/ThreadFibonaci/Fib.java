3ac50a128b89a2f146748a2b24b3f8033eafc8d3
Scruf <ekozitski@hotmail.com>
2016-09-15 20:48:02 -0400

    Finalized Fibonacci Numbers Thread

--- DistributedSystems/ThreadFibonaci/Fib.java
+++ DistributedSystems/ThreadFibonaci/Fib.java
@@ -1,123 +1,151 @@
-import java.math.*;
-import java.io.*;
-/*
-	Implement a working version of compareTo
-*/
 
-class Print extends Fib implements Runnable{
-	Print(int n){
-		super(n);
-		
-	}
+import java.math.BigInteger;
 
-	@Override
-	public void run(){
-		try{
-			System.out.printf("Printing element %s ",super.getValue(super.F.length-1));
-		}catch(InterruptedException ie){
-			ie.printStackTrace();
-		}
-	}
+/**
+ * main method. read the inputs from command line and then provide a new thread to output and calculate every one field.
+ * @author Egor Kozitski
+ */
+public class Fib {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.err.printf("%d arguments supplied but 3 required\n",args.length);
+            System.exit(0);
+        }else {
+        	BigInteger a = new BigInteger("0");
+        	BigInteger b = new BigInteger("1");
+        	int n = 0;
+            try{
+            	a = new BigInteger(args[0]);
+            	b = new BigInteger(args[1]);
+            	n = Integer.valueOf(args[2]);
+            }
+            catch(NumberFormatException num){
+            	System.err.print("Arguments must be of type Int\n");
+            	System.exit(0);
+            }
+           Monitor monitor = new Monitor(n + 1);
+        new OutputThread(monitor, n).start();
+        for(int index = n; index >= 0; index--) {
+            if (index == 0) {
+                new CalculateFirstNumbersThread(monitor, index, a).start();
+            } else if (index == 1) {
+                new CalculateFirstNumbersThread(monitor, index, b).start();
+            } else {
+                new CalculateThread(monitor, index).start();
+            }
+        }
+
+        }
+
+    }
+}
+
+/**
+ * print the result
+ */
+class OutputThread extends Thread {
+    volatile Monitor monitor;
+    int index;
+
+    public OutputThread(Monitor m, int index) {
+        this.monitor = m;
+        this.index = index;
+    }
+    @Override
+    public void run() {
+        while (monitor.getValue(index) == null) {
+            try {
+                sleep(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println(monitor.getValue(index));
+
+    }
+}
+
+/**
+ * calculate the number in according index
+ */
+class CalculateThread extends Thread {
+    volatile Monitor monitor;
+    int index;
+    BigInteger number;
+
+    public CalculateThread(Monitor m, int index) {
+        this.monitor = m;
+        this.index = index;
+    }
+
+    @Override
+    public void run() {
+
+        while (monitor.getValue(index - 1) == null || monitor.getValue(index - 2) == null) {
+             try {
+                 sleep(1);
+             } catch (InterruptedException ex) {
+                 ex.printStackTrace();
+             }
+        }
+
+        monitor.putValue(index, monitor.getValue(index - 1).add(monitor.getValue(index - 2)));
+    }
+}
+
+/**
+ * calculate first two numbers in the array
+ */
+class CalculateFirstNumbersThread extends Thread {
+    private Monitor monitor;
+    private int index;
+    private BigInteger value;
+
+    public CalculateFirstNumbersThread(Monitor m, int i, BigInteger v) {
+        this.monitor = m;
+        this.index = i;
+        this.value = v;
+    }
+
+    @Override
+    public void run() {
+        monitor.putValue(index, value);
+    }
 }
 
 
 
-class Compute extends Fib implements Runnable{
-	BigInteger a;
-	BigInteger b;
-	int n;
-	Compute (Integer a, Integer b,int n){
-		super(n);
-		this.a = BigInteger.valueOf(a);
-		this.b = BigInteger.valueOf(b);
-		this.n =  n;
-		try{
-			super.putValue(0,this.a);
-			super.putValue(1,this.b);
-		}catch (InterruptedException ie){
+class Monitor {
+    volatile BigInteger[] F;
 
-		}
-		
-	}
+    /**
+     * initializes the monitor to calculate the generalized Fibonacci number for argument n
+     * @param n size of the list
+     */
+    public Monitor(int n) {
+        F = new BigInteger[n];
+    }
 
-	@Override
-	public void run(){
-	
-		if(this.a.compareTo(this.b)==1){
-			System.err.print("Second argument must be greater than first\n");
-			System.exit(0);
-		}
-			try{
-				
-			
-				BigInteger next = super.getValue(this.n-2).add(super.getValue(this.n-1));
-				
-				super.putValue(n,next);
-			}catch (InterruptedException ie){
-				ie.printStackTrace();
-			}	
-					
-		
-	}
-}
+    /**
+     * puts the given value into array element i
+     * @param i index of the element where  element will be put
+     * @param value value which will be putted into the array
+     */
+    synchronized void putValue(int i, BigInteger value) {
+        if (F[i] == null) {
+            F[i] = value;
+        } else {
+            System.err.println("A value has already been put into array element i");
+        }
+    }
+
+    /**
+     * @param i index of the element to be returned
+     * @return the value stored in array element i
+     */
+    BigInteger getValue(int i) {
+        return F[i];
+    }
 
 
-
-public class Fib{
-	
-	public BigInteger F[];
-	int n;
-	Fib(int n){
-		if(n<0){
-			System.err.print("N must be >= 0\n");
-			System.exit(0);
-		}else{
-			F = new BigInteger[n+1];
-			this.n = n;	
-		}
-		
-	
-	}
-	public synchronized void putValue(int i, BigInteger element)throws InterruptedException{
-		if (F[i] == null){
-			F[i] = element;
-			System.out.printf("Tying to put %s \n",element.toString());
-			notifyAll();
-		}
-		else{
-			System.err.printf("Element at position %d exists in the list\n",i);
-			System.exit(0);
-		}
-	}
-	public synchronized BigInteger getValue(int i) throws InterruptedException{
-		while(F[i]==null){
-			System.out.printf("Still stuck in %d-> ",i);
-			System.out.println(F[i]);
-			wait();
-		}
-		return F[i];
-	}
-
-	public static void main(String[] args) {
-		if (args.length<3){
-			System.err.print("Invalid number of argumnets\n");
-			System.exit(0);
-		}
-		Integer n = 0;
-		Integer a = 0;
-		Integer b = 0;
-		try{
-			n = Integer.parseInt(args[2]);
-			a = Integer.parseInt(args[0]);
-			b = Integer.parseInt(args[1]);
-		}catch(Exception ex){
-			System.err.print("Invalid type of command line argumnents \n");
-			System.exit(0);
-		}
-		Fib fib  = new Fib(n);
-		new Thread( new Print(n)).start();
-		for(int i=n;i>=0;i--){
-			new Thread(new Compute(a,b,n)).start();
-		}
-	}	
 }
