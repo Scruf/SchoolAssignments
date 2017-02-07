b3022aa616507cb3f679c4dd8e99ccd89bf4bd22
Egor <ekozitski@hotmail.com>
2017-02-06 21:19:28 -0500

    Update assignment1.scm

--- CSCI 344/assignment1.scm
+++ CSCI 344/assignment1.scm
@@ -1,11 +1,24 @@
 #lang scheme
+#1
 (define 2nd
   (lambda (xs)
     (cadr xs)))
-
+#2
 (define one?
   (lambda (xs)
    (cond
      [(null? xs) #f]
      [(null? (cdr xs)) #t]
          [else #f])))
+
+
+#4
+(define index
+  (lambda (sym syms)
+    (cond
+      [(null? syms) #f]
+      [(eq? sym (car syms)) 0]
+      [(eq? (index sym (cdr syms)) #f) #f]
+      [else (+ 1 (index sym (cdr syms)))])))
+
+     
