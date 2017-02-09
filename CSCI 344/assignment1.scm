4566aa27025afed236a5a34e6bf699629a428d94
Egor <ekozitski@hotmail.com>
2017-02-08 23:50:13 -0500

    Update assignment1.scm

--- CSCI 344/assignment1.scm
+++ CSCI 344/assignment1.scm
@@ -1,9 +1,14 @@
 #lang scheme
-#1
+;CSCI Assignment 1
+;author Egor Kozitski
+
+;1 @function 2nd returns the second element of the list
+;  @param xs -> List
 (define 2nd
   (lambda (xs)
     (cadr xs)))
-#2
+;2 @function return #t if the list has exactly one element
+;  @param xs -> List
 (define one?
   (lambda (xs)
    (cond
@@ -12,7 +17,8 @@
          [else #f])))
 
 
-#4
+;4 @function index finds the index of the element in the list
+;  @param sym -> Symbol syms-> List of Symbols
 (define index
   (lambda (sym syms)
     (cond
@@ -22,7 +28,9 @@
       [else (+ 1 (index sym (cdr syms)))])))
 
      
-;3
+;3 helper
+; @function helper for insertion sort
+; @param xs-> Number ys->List of numbers
 (define insert
   (lambda (xs ys)
     (cond
@@ -30,16 +38,77 @@
      [(<= xs (car ys))(cons xs ys)]
      [else (cons (car ys)(insert xs (cdr ys)))])))
 
-
+;3 @function insertion-sort will sort elements in
+; ascnding order
+;  @param ys -> List of numbers
 (define insertion-sort
   (lambda (ys)
     (cond
       [(null? ys) '()]
       [else (insert (car ys)(insertion-sort (cdr ys)))])))
-
+;5  @function filter-by will return list based on the predicate
+;   @params p -> Predicate L-> List
 (define filter-by
   (lambda (p L)
     (cond
       [(null? L) '()]
       [(p (car L)) (cons  (car L) (filter-by p (cdr L)))]
       [else (filter-by p (cdr L))])))
+
+
+;5 @function find-less
+(define find-less
+  (lambda (xs ys)
+    (filter-by (lambda (x) (< x xs)) ys)))
+
+;5 @function find-same
+(define find-same
+  (lambda (xs ys)
+    (filter-by (lambda (x) (= x xs)) ys)))
+
+;5 @find-more
+(define find-more
+  (lambda (xs ys)
+    (filter-by (lambda (x) (> x xs)) ys)))
+
+
+(display "#1 Testing 2nd\n")
+
+(equal? (2nd '(x y)) 'y)
+(equal? (2nd '(a a a a a)) 'a)
+
+(display "#2 Testing one?\n")
+
+(equal? (one? '(s)) #t)
+(equal? (one? '()) #f)
+
+(display "#3 Testing insertion-sort\n")
+
+(equal? (insertion-sort '()) '())
+(equal? (insertion-sort '(3 1 2 5 4)) '(1 2 3 4 5))
+
+(display "#4 Testing index\n")
+
+(equal? (index '2 '(1 2 4 5)) 1)
+(equal? (index 'x '(q r s x y z)) 3)
+
+(display "#5 Testing filter-by\n")
+
+(equal? (filter-by symbol? '(() a 5 (1 2 3) (x y z) b 10)) '(a b))
+(equal? (filter-by number? '(() a 5 (1 2 3) (x y z) b 10)) '(5 10))
+
+(display "Testing find-less\n")
+
+(equal? (find-less '3 '(1 2 4 5)) '(1 2))
+(equal? (find-less '4 '(5 5 5 5)) '())
+
+(display "Testing find-same\n")
+
+(equal? (find-same '3 '(1 3 4 5)) '(3))
+(equal? (find-same '2 '(1 4 5 6 )) '())
+
+(display "Testing find-more\n")
+
+(equal? (find-more '3 '(1 233 4 5 )) '(233 4 5))
+(equal? (find-more '2 '(4 5 6 6 7)) '(4 5 6 6 7))
+
