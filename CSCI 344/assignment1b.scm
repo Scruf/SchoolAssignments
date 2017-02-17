cba40107ec2b8cc81c87f189dbac98d1c2f4bc0a
Egor <ekozitski@hotmail.com>
2017-02-17 12:36:38 -0500

    Update assignment1b.scm

--- CSCI 344/assignment1b.scm
+++ CSCI 344/assignment1b.scm
@@ -5,3 +5,14 @@
       [(null? order2) order1]
       [(>= (car order1)(car order2))(cons (car order2)(merge order1 (cdr order2))) ]
       [else (cons (car order1)(merge (cdr order1) order2))])))
+
+
+(define even
+  (lambda (xs)
+    (cond
+      [(null? xs) '()]
+      [(null? (cdr xs)) (cons (car xs) '())]
+      [else (cons (car xs)(even (cdr (cdr xs))))])))
+
+
+
