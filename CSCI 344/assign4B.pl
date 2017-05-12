9901dd7c49718c48603f3b9c45d12cbf9f80281d
Egor <ekozitski@hotmail.com>
2017-05-12 11:15:29 +0000

    Update assign4B.pl

--- CSCI 344/assign4B.pl
+++ CSCI 344/assign4B.pl
@@ -1,60 +1,61 @@
-%% judgeType(Gamma, N, int) :- number(N).
+%Egor Kozitski
+judgeType(Gamma, N, int) :- number(N).
 
-%% judgeType(Gamma, X, T) :- atom(X), lookUp(X, Gamma, T).
+judgeType(Gamma, X, T) :- atom(X), lookUp(X, Gamma, T).
 
-%% judgeType(Gamma, if(M, N1, N2), T) :-
-%%         judgeType(Gamma, M, bool),
-%%         judgeType(Gamma, N1, T),
-%%         judgeType(Gamma, N2, T).
+judgeType(Gamma, if(M, N1, N2), T) :-
+        judgeType(Gamma, M, bool),
+        judgeType(Gamma, N1, T),
+        judgeType(Gamma, N2, T).
 
-%% judgeType(Gamma, let(X, M, N), T) :-
-%%         judgeType(Gamma, M, T1),
-%%         extend(Gamma, X, T1, Gamma2),
-%%         judgeType(Gamma2, N, T).
+judgeType(Gamma, let(X, M, N), T) :-
+        judgeType(Gamma, M, T1),
+        extend(Gamma, X, T1, Gamma2),
+        judgeType(Gamma2, N, T).
 
-%% judgeType(Gamma, letRec(Y, X, TX, TM, M, N), T) :-
-%%         extend(Gamma, X, TX, Gamma2),
-%%         extend(Gamma2, Y, arrow(TX, TM), Gamma3),
-%%         judgeType(Gamma3, M, TM),
-%%         extend(Gamma, Y, arrow(TX, TM), Gamma4),
-%%         judgeType(Gamma4, N, T).
+judgeType(Gamma, letRec(Y, X, TX, TM, M, N), T) :-
+        extend(Gamma, X, TX, Gamma2),
+        extend(Gamma2, Y, arrow(TX, TM), Gamma3),
+        judgeType(Gamma3, M, TM),
+        extend(Gamma, Y, arrow(TX, TM), Gamma4),
+        judgeType(Gamma4, N, T).
 
-%% judgeType(Gamma, proc(X, T, M), arrow(T, TM)) :-
-%%         extend(Gamma, X, T, Gamma2),
-%%         judgeType(Gamma2, M, TM).
+judgeType(Gamma, proc(X, T, M), arrow(T, TM)) :-
+        extend(Gamma, X, T, Gamma2),
+        judgeType(Gamma2, M, TM).
 
-%% judgeType(Gamma, funcall(M, N), T) :-
-%%         judgeType(Gamma, N, TN),
-%%         judgeType(Gamma, M, arrow(TN, T)).
+judgeType(Gamma, funcall(M, N), T) :-
+        judgeType(Gamma, N, TN),
+        judgeType(Gamma, M, arrow(TN, T)).
 
-%% judgeType(Gamma, nil(T), list(T)).
+judgeType(Gamma, nil(T), list(T)).
 
-%% judgeType(Gamma, cons(M, N), list(T)) :-
-%%     judgeType(Gamma, N, T),
-%%     judgeType(Gamma, M, list(T)).
+judgeType(Gamma, cons(M, N), list(T)) :-
+    judgeType(Gamma, N, T),
+    judgeType(Gamma, M, list(T)).
                                
-%% judgeType(Gamma, isNil(M), bool) :-
-%%     judgeType(Gamma, M, list(T)). 
+judgeType(Gamma, isNil(M), bool) :-
+    judgeType(Gamma, M, list(T)). 
                       
-%% judgeType(Gamma, head(M), list(T)) :-
-%%     judgeType(Gamma, M, T).
+judgeType(Gamma, head(M), list(T)) :-
+    judgeType(Gamma, M, T).
     
-%% judgeType(Gamma, tail(M), list(T)) :-
-%%     judgeType(Gamma, M, list(T)).
+judgeType(Gamma, tail(M), list(T)) :-
+    judgeType(Gamma, M, list(T)).
 
 
-%% lookUp(X, [[X, Type] | Tail], Type) :- !.
-%% lookUp(X, [[Y, T] | Tail], Type) :- 
-%%     lookUp(X, Tail, Type).
+lookUp(X, [[X, Type] | Tail], Type) :- !.
+lookUp(X, [[Y, T] | Tail], Type) :- 
+    lookUp(X, Tail, Type).
 
-%% extend(Gamma, X, T, [[X, T] | Gamma]).
+extend(Gamma, X, T, [[X, T] | Gamma]).
 
-%% initGamma([
-%%     [eq, arrow(int, arrow(int, bool))],
-%%     [prod, arrow(int, arrow(int, int))],
-%%     [sum, arrow(int, arrow(int, int))],
-%%     [diff, arrow(int, arrow(int, int))]
-%%           ]).
+initGamma([
+    [eq, arrow(int, arrow(int, bool))],
+    [prod, arrow(int, arrow(int, int))],
+    [sum, arrow(int, arrow(int, int))],
+    [diff, arrow(int, arrow(int, int))]
+          ]).
 
 /*
  * Derivative
@@ -64,25 +65,39 @@ d(X, X, 1).
 d(C, X, 0) :- atomic(C), C \= X.
 d(U + V, X, DU + DV) :- d(U, X, DU), d(V, X, DV).
 d(U * V, X, U * DV + V * DU) :- d(U, X, DU), d(V, X, DV).
+partition([],P,Less,Same,Greater):- Less = [], Same = [], Greater = [].
+partition([H|T],P,Less1,Same1,Greater1):-
+	H=P,
+	partition(T,P,Less2,Same2,Greater2),
+	Less1 = Less2,
+	Same1 = [H |Same2],
+	Greater1 = Greater2.
+partition([H|T], P, Less1, Same1, Greater1):-
+	H<P,
+	partition(T,P,Less2,Same2,Greater2),
+	Less1 = [H |Less2],
+	Same1 = Same2,
+	Greater1 = Greater2.
+partition([H|T],P,Less1,Same1,Greater1):-
+	H>P,
+	partition(T,P,Less2,Same2,Greater2),
+	Less1 = Less2,
+	Same1 = Same2,
+	Greater1 = [H | Greater2].
 
 
+%quicksort.pl quicksort
 quicksort([], []).
 quicksort([H | T], S) :-
    partition(T, H, Less, Same, Greater),
    quicksort(Less, SLess),
    quicksort(Greater, SGreater),
-   append(SLess, Same, SGreater, S).
-                       
+   append(SLess, [H | Same], SGreater, S).
+
 append([], L, L).
 append([H | T], L, [H | A]) :- append(T, L, A).
 
-append(L1, L2, L3, A) :- append(L2, L3, L23), append(L1, L23, A).
-                      
-
-partition([], pivot, [], []).
-partition([H|T], pivot, SLess, Same, [H|SGreater]) :- H > pivot, partition(T, pivot, SLess, Same, SGreater).
-partition([H|T], pivot, [H|SLess], Same, SGreater) :- H < pivot, partition(T, pivot, SLess, Same, SGreater).
-partition([H|T], pivot, SLess, [H|Same], SGreater) :-  H = pivot, partition(T, pivot, SLess, Same, SGreater).
+append(L1, L2, L3, A) :- append(L2, L3, L23), append(L1, L23, A).                   
 
 
 merge([], XS, XS).
